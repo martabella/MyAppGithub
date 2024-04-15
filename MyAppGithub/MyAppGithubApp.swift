3ced8c7986ccb08d83c4ad066cbee7238b7ae759
martabella <martabella@users.noreply.github.com>
2024-04-15 13:13:51 +0200

    Uploading initial files

---
+++ MyAppGithub/MyAppGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppGithubApp.swift
+//  MyAppGithub
+//
+//  Created by LSOnline on 15/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyAppGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
